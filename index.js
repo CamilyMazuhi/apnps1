d54db9548dd06d0d28db30f0c9e43e42567057f2
camily mazuhi <camily.martins24@gmail.com>
2023-03-27 16:59:00 -0300

    novas alterações
    Signed-off-by: camily mazuhi <camily.martins24@gmail.com>

---
+++ index.js
@@ -0,0 +1,29 @@
+const express= require ('express')
+const app=express ()
+var bodyParser= require('body-parser')
+var cookieParser= require('cookie-parser')
+var path=require ('path')
+
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set ("view engine","ejs")
+
+app.use(express.static(path.join(__dirname,"public")))
+
+
+app.get('/',function(req,res){
+ res.render ('index.ejs',{})
+})
+
+app.get('/usuarios',function(req,res){
+    res.render ('usuarios.ejs',{usuarios:['Diego','Maria','Tereza','Ana','Lucia','José']})
+   }) 
+
+app.listen(3000, function(){
+    
+    console.log("Conexão inicializada")
+
+})
+
