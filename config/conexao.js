7995e029eec9531eab6f772b9790d77164785c59
camily mazuhi <camily.martins24@gmail.com>
2023-03-28 21:05:26 -0300

    modificação
    Signed-off-by: camily mazuhi <camily.martins24@gmail.com>

--- config/conexao.js
+++ config/conexao.js
@@ -1,6 +1,6 @@
 const mongoose =require ('mongoose')
 const uri= "mongodb://diego:diego@ac-pcra3vd-shard-00-00.ajiapmy.mongodb.net:27017,ac-pcra3vd-shard-00-01.ajiapmy.mongodb.net:27017,ac-pcra3vd-shard-00-02.ajiapmy.mongodb.net:27017/?ssl=true&replicaSet=atlas-14e4b2-shard-0&authSource=admin&retryWrites=true&w=majority"
 
-mongoose.connect(uri)
+mongoose.connect(uri,{useNewUrlParser:true,useUnifiedtopology:true})
 
 module.exports=mongoose
